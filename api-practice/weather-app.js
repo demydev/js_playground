4d16fde3861a48059bb01a626c66eca9549de48b
belarr0 <belarr00ss@gmail.com>
2026-09-17 10:25:06 +0200

    upg insert method

---
+++ api-practice/weather-app.js
@@ -0,0 +1,16 @@
+//const API_KEY = '4f31e624bd29447003961cfe8a44105d';
+//const CITY = 'Warsaw';
+//const URL = `https://api.openweathermap.org/data/2.5/weather?q=${CITY}&units=metric&appid=${API_KEY}`;
+
+async function getWeather(CITY) {
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${CITY}&units=metric&appid=4f31e624bd29447003961cfe8a44105d`);
+    if (!response.ok){
+        console.error(`Error: ${response.status} ${response.statusText}`);
+        return;
+    }
+    const data = await response.json();
+    console.log(JSON.stringify(data, null, 1))
+    
+}
+
+getWeather('Lodz');
